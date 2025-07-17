6eddd6b3950d070dfb6c909acca7dbcc4bc6c6c0
Lucas Silva <63220410+silva021@users.noreply.github.com>
2025-07-17 12:42:46 -0300

    feat: changes category type colors

--- app/src/main/java/com/silva021/tanalista/domain/model/CategoryType.kt
+++ app/src/main/java/com/silva021/tanalista/domain/model/CategoryType.kt
@@ -15,8 +15,8 @@ enum class CategoryType(
     val color: Color
 ) {
     GROCERY("Mercado", Icons.Default.ShoppingCart, Color(0xFF4CAF50)),
-    PET("Pet", Icons.Default.Pets, Color(0xFFFFC107)),
+    PET("Pet", Icons.Default.Pets, Color(0xFFFFB300)),
     CLEANING("Limpeza", Icons.Default.DeleteSweep, Color(0xFF2196F3)),
-    PHARMACY("Farmácia", Icons.Default.MedicalServices, Color(0xFFF44336)),
-    OTHER("Outros", Icons.Default.CreditCard, Color(0xFF9E9E9E))
+    PHARMACY("Farmácia", Icons.Default.MedicalServices, Color(0xFFD32F2F)),
+    OTHER("Outros", Icons.Default.CreditCard, Color(0xFF757575))
 }
--- app/src/main/java/com/silva021/tanalista/domain/usecase/GetShoppingListByIdUseCase.kt
+++ app/src/main/java/com/silva021/tanalista/domain/usecase/GetShoppingListByIdUseCase.kt
@@ -3,6 +3,7 @@ package com.silva021.tanalista.domain.usecase
 import com.google.firebase.Firebase
 import com.google.firebase.crashlytics.crashlytics
 import com.silva021.tanalista.data.datastore.FireStoreHelper.getShoppingItemsCollection
+import com.silva021.tanalista.data.datastore.FireStoreHelper.shoppingListCollection
 import com.silva021.tanalista.data.dto.ShoppingListDTO
 import com.silva021.tanalista.domain.mappers.toModel
 import com.silva021.tanalista.domain.model.ShoppingList
@@ -15,16 +16,16 @@ class GetShoppingListByIdUseCase() {
         onFailure: () -> Unit,
     ) {
         try {
-            val query = getShoppingItemsCollection(listId)
-                .whereEqualTo("listId", listId)
+            val query = shoppingListCollection
+                .document(listId)
                 .get()
                 .await()
 
-            val shoppingList = query.documents.firstNotNullOf {
-                it.toObject(ShoppingListDTO::class.java)?.copy(id = listId)
-            }.toModel()
-
-            onSuccess.invoke(shoppingList)
+            query.toObject(ShoppingListDTO::class.java)?.copy(id = listId)?.toModel()?.let {
+                onSuccess.invoke(it)
+            } ?: run {
+                throw Exception("Shopping list not found")
+            }
         } catch (e: Exception) {
             Firebase.crashlytics.recordException(e)
             e.printStackTrace()
